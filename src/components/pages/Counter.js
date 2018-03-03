4870640bb3dd3a4cf1f5cdb3731e5d83e937da88
jbpark <jbpark@wemakeprice.com>
2018-03-03 17:17:28 +0900

    update counter component

--- src/components/pages/Counter.js
+++ src/components/pages/Counter.js
@@ -2,15 +2,50 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as counterActions from '../../redux/modules/counter';
+import * as postActions from '../../redux/modules/post';
 
 class Counter extends React.Component {
+  componentWillMount() {
+    const { counter } = this.props;
+    this.getPost(counter);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.counter !== nextProps.counter) {
+      this.getPost(nextProps.counter);
+    }
+  }
+
+  getPost = async (postId) => {
+    const { PostActions } = this.props;
+
+    try {
+      await PostActions.getPost(postId);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
-    const { counter, CounterActions } = this.props;
+    const { post, counter, CounterActions } = this.props;
+    const result = post.toJS();
+
     return (
       <div>
       <h1>{counter}</h1>
       <button onClick={CounterActions.increment}>+</button>
       <button onClick={CounterActions.decrement}>-</button>
+      {
+        result.pending ?
+          <h2>Loading...</h2> :
+          result.error ?
+            <h1>Error!</h1> : (
+              <div>
+                  <h1>{result.data.title}</h1>
+                  <p>{result.data.body}</p>
+              </div>
+          )
+      }
       </div>
     );
   }
@@ -19,8 +54,10 @@ class Counter extends React.Component {
 export default connect(
   (state) => ({
     counter: state.counter,
+    post: state.post
   }),
   (dispatch) => ({
     CounterActions: bindActionCreators(counterActions, dispatch),
+    PostActions: bindActionCreators(postActions, dispatch)
   })
 )(Counter);
--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
+import post from './post';
 export default combineReducers({
-  counter
+  counter,
+  post
 });
