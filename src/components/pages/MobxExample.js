968d3139ecee97a3b0426833b36db78a8cfca181
jbpark <jbpark@wemakeprice.com>
2018-03-09 11:23:08 +0900

    update mobx/store.js for mobx async actions

--- src/components/pages/MobxExample.js
+++ src/components/pages/MobxExample.js
@@ -4,12 +4,19 @@ import { observer, inject } from 'mobx-react';
 @inject('store')
 @observer
 class MobxExample extends React.Component {
+
+  componentWillMount() {
+    const { store } = this.props;
+    store.fetchPostData(store.counter);
+  }
+
   render() {
     const { store } = this.props;
+    const { counter, post } = store;
 
     return (
       <div>
-        <h1>{ store.counter }</h1>
+        <h1>{ counter }</h1>
         <button
           onClick={store.increase}>
           +
@@ -18,6 +25,17 @@ class MobxExample extends React.Component {
           onClick={store.decrease}>
           -
         </button>
+        {
+          post.status === 'pending' ?
+            <h2>Loading...</h2> :
+            post.status === 'error' ?
+              <h1>Error!</h1> : (
+                <div>
+                    <h1>{post.data.title}</h1>
+                    <p>{post.data.body}</p>
+                </div>
+            )
+        }
       </div>
     );
   }
--- src/mobx/Store.jsx
+++ src/mobx/Store.jsx
@@ -1,16 +1,39 @@
 import { observable, action } from 'mobx';
+import axios from 'axios';
 
 class Store {
-  @observable counter = 0;
+  @observable counter = 1;
+  @observable post = {
+    status: 'pending',
+    data: [],
+  };
 
   @action
   increase = () => {
-    this.counter++;
+    this.fetchPostData(++this.counter);
   }
 
   @action
   decrease = () => {
-    this.counter--;
+    this.fetchPostData(--this.counter);
+  }
+
+  @action
+  fetchPostData = async (id) => {
+    this.post.status = 'pending';
+    await this.getPost(id).then(
+      res => {
+        this.post.status = 'done';
+        this.post.data = res.data;
+      },
+      error => {
+        this.post.status = 'error';
+      }
+    )
+  }
+
+  getPost(postId) {
+    return axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`);
   }
 }
 
